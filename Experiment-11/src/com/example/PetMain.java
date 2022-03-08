af7b926ffe18adfe56281dda38d51afcc0ccad66
Prasoon Ghosh <86545043+prasoonghosh@users.noreply.github.com>
2022-03-08 12:30:19 +0530

    Update PetMain.java

--- Experiment-11/src/com/example/PetMain.java
+++ Experiment-11/src/com/example/PetMain.java
@@ -1,5 +1,51 @@
 package com.example;
 
 public class PetMain {
-    
+    public static void playWithAnimal(Animal a) {
+        if(a instanceof Pet) {
+            Pet p = (Pet)a;
+            p.play();
+        } else {
+            System.out.println("Danger! Wild Animal");
+        }
+    }
+    public static void main(String[] args) {
+        Animal a;
+        Spider s=new Spider();
+        s.eat();
+        s.walk();
+        a=new Spider();
+        a.eat();
+        a.walk();
+        System.out.println("-------------");
+        System.out.println();
+
+        Pet p;
+        Cat c = new Cat("Tom");
+        c.eat();
+        c.walk();
+        c.play();
+        a = new Cat();
+        a.eat();
+        a.walk();
+        System.out.println("--------------");
+        System.out.println();
+
+        p = new Cat();
+        p.setName("Mr. Whiskers");
+        p.play();
+        Fish f = new Fish();
+        f.setName("Guppy");
+        f.eat();
+        f.walk();
+        f.play();
+        a=new Fish();
+        a.eat();
+        a.walk();
+
+
+        playWithAnimal(s);
+        playWithAnimal(c);
+        playWithAnimal(f);
+    }
 }
